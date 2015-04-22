5762ee66541c7aa4078bd39fa7b6572ebbfc9b73
janmilosh <janmilosh@gmail.com>
2015-04-21 21:33:34 -0400

    Adds some example routes

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/badbadbad')
+def bad():
+    return '<h1>Bad Bad Request</h1>', 400
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
