ed9c81d82a8d3288ccb995472ff6d5d181965545
janmilosh <janmilosh@gmail.com>
2015-04-22 22:22:26 -0400

    Adds several templates

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, make_response, redirect, abort, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/user_agent')
 def user_agent():
@@ -13,9 +15,10 @@ def user_agent():
 def index():
     return render_template('index.html')
 
+
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', first_name=name)
+    return render_template('user.html', name=name)
 
 @app.route('/badbadbad')
 def bad():
@@ -30,6 +33,14 @@ def cookie():
 @app.route('/redirectme')
 def redirectme():
     return redirect('http://janmilosh.com')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
